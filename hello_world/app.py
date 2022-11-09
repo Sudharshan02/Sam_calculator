1d580f743256ec38a786671b9d04eb3410d87257
Sudharshan02 <naiksudharshan555@gmail.com>
2022-11-09 17:14:14 +0530

    initial commit

---
+++ hello_world/app.py
@@ -0,0 +1,30 @@
+import json
+
+# import requests
+
+
+def lambda_handler(event, context):
+
+    x = event["x"]
+    y = event["y"]
+
+    print(sum(x,y))
+    print(subtract(x,y))
+    print(multiply(x,y))
+    print(divide(x,y))
+
+
+
+def sum(x, y):
+    return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError('Can not divide by Zero!')
+    return x/y
