156f8d0449465d053b113956cc45601e1cd15962
Sudharshan02 <60228673+Sudharshan02@users.noreply.github.com>
2022-11-10 00:44:07 +0530

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -3,6 +3,34 @@
 import unittest
 
 from hello_world import app
+
+def lambda_handler(event, context):
+
+    x = event["x"]
+    y = event["y"]
+
+    print(sum(x,y))
+    print(subtract(x,y))
+    print(multiply(x,y))
+    print(divide(x,y))
+
+
+
+def sum(x, y):
+    return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError('Can not divide by Zero!')
+    return x/y
+
+
 class calculator_Test(unittest.TestCase):
 
     def setUp(self):
@@ -20,7 +48,7 @@ class calculator_Test(unittest.TestCase):
         #x = 10
         #y = 20
         #act
-        #result = sum(self.x, self.y)
+        result = sum(self.x, self.y)
         result1 = subtract(self.x, self.y)
         result2 = multiply(self.x, self.y)
         result3 = divide(self.x, self.y)
@@ -28,7 +56,7 @@ class calculator_Test(unittest.TestCase):
 
         #Assert
 
-        #self.assertEqual(result, self.x + self.y)
+        self.assertEqual(result, self.x + self.y)
         self.assertEqual(result1, self.x - self.y)
         self.assertEqual(result2, self.x * self.y)
         self.assertEqual(result3, self.x / self.y)
@@ -41,14 +69,14 @@ class calculator_Test(unittest.TestCase):
         #x = 10
         #y = 20
         # act
-        #result = sum(self.y, self.x)
+        result = sum(self.y, self.x)
         result1 = subtract(self.y, self.x)
         result2 = multiply(self.y, self.x)
         result3 = divide(self.y, self.x)
 
         # Assert
 
-        #self.assertEqual(result, self.y + self.x)
+        self.assertEqual(result, self.y + self.x)
         self.assertEqual(result1, self.y - self.x)
         self.assertEqual(result2, self.y * self.x)
         self.assertEqual(result3, self.y / self.x)
